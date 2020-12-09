3eb6910fdd5b0db881fa835e44e04c75130d1030
Lucas Santos <lucasa95santos@gmail.com>
2020-12-08 20:54:42 -0400

    chore: configurando a conexão com o MongoDB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const routes = require('./routes');
 const server = express();
 const PORT = 8080;
+
+// conexão com o MongoDB
+mongoose.connect('mongodb+srv://lucas_santos:l08a04s95@implementacao-mongo.zxpo5.mongodb.net/db_implementacao?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 server.use(cors());
 server.use(express.json());
