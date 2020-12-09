e0ff57e0d093d8b4328fa4efe3e0abf6e596e41c
Lucas Santos <lucasa95santos@gmail.com>
2020-12-08 21:20:48 -0400

    feat: criando o controller de desenvolvedor e implementando o cadastro

---
+++ src/controllers/DeveloperController.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+// model
+const Developer = require('../models/Developer');
+
+class DeveloperController {
+  async create(request, response) {
+    const { username } = request.body;
+
+    const userExists = await Developer.findOne({ user: username });
+
+    if (userExists) {
+      return response.json(userExists);
+    }
+
+    const githubResponse = await axios.get(`https://api.github.com/users/${username}`);
+    const { name, bio, avatar_url } = githubResponse.data;
+
+    const dev = await Developer.create({
+      name: name !== null ? name : "Sem nome",
+      user: username,
+      bio: bio !== null ? bio : "Sem bio",
+      avatar: avatar_url
+    });
+
+    return response.json(dev);
+  }
+}
+
+module.exports = new DeveloperController();
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
+// controllers
+const developerController = require('./controllers/DeveloperController');
 
 const routes = Router();
+
+routes.post('/devs', developerController.create);
 
 module.exports = routes;
