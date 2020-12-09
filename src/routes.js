5706dcad8d7f5849ea716c8e24022e55788b4901
Lucas Santos <lucasa95santos@gmail.com>
2020-12-08 22:18:36 -0400

    feat: implementando os métodos de like e dislike em um desenvolvedor

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,13 @@ const developerController = require('./controllers/DeveloperController');
 
 const routes = Router();
 
+// rota para cadastrar um desenvolvedor
 routes.post('/devs', developerController.create);
+
+// rota para dar like em algum desenvolvedor
+routes.post('/devs/:developerId/likes', developerController.like);
+
+// rota para dar dislike em algum desenvolvedor
+routes.post('/devs/:developerId/dislikes', developerController.dislike);
 
 module.exports = routes;
